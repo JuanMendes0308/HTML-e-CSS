2d0a27837a8173f51953815e4f0af308014267c5
Juan Mendes <mendesj0308@gmail.com>
2026-02-11 23:49:54 -0300

    Envio de repositório

---
+++ 1- HTML e CSS/Filme Avatar/informacao.js
@@ -0,0 +1,9 @@
+function clicar(){
+    let idade = window.document.getElementById('idade');
+    let masculino = window.document.getElementById('masculino');
+    let feminino = window.document.getElementById('feminino');
+    let excelente = window.document.getElementById('excelente');
+    let bom = window.document.getElementById('bom');
+    let regular = window.document.getElementById('regular');
+    let pessimo = window.document.getElementById('pessimo');
+}
